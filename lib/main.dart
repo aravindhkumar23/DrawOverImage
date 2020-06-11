cfc400caf9af0eea76d160d50f6c0d3c63b37941
Aravindh Kumar <aravindhkumar23@gmail.com>
2020-06-11 09:56:45 +0530

    view port cleanup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'dart:io';
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 //import 'package:path_provider/path_provider.dart';
 //import 'package:simple_permissions/simple_permissions.dart';
@@ -12,43 +12,47 @@ const directoryName = 'Signature';
 
 void main() {
   runApp(MaterialApp(
-    home: SignApp(),
+    home: ImageEditor(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-class SignApp extends StatefulWidget {
+class ImageEditor extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return SignAppState();
+    return ImageEditorState();
   }
 }
 
-class SignAppState extends State<SignApp> {
-  GlobalKey<SignatureState> signatureKey = GlobalKey();
+class ImageEditorState extends State<ImageEditor> {
+  GlobalKey<CustomDrawViewState> signatureKey = GlobalKey();
   var image;
-  String _platformVersion = 'Unknown';
-
-//  Permission _permission = Permission.WriteExternalStorage;
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Signature(key: signatureKey),
+      body: CustomDrawView(key: signatureKey),
       persistentFooterButtons: <Widget>[
-        FlatButton(
-          child: Text('Clear'),
+        IconButton(
+          icon: Icon(Icons.slideshow),
+          onPressed: () {
+
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.color_lens),
+          onPressed: () {
+
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.clear),
           onPressed: () {
             signatureKey.currentState.clearPoints();
           },
         ),
-        FlatButton(
-          child: Text('Save'),
+        IconButton(
+          icon: Icon(Icons.save),
           onPressed: () {
             // Future will resolve later
             // so setState @image here and access in #showImage
@@ -63,7 +67,6 @@ class SignAppState extends State<SignApp> {
   setRenderedImage(BuildContext context) async {
     ui.Image renderedImage = await signatureKey.currentState.rendered;
 
-    print('image ${renderedImage.toString()}');
     setState(() {
       image = renderedImage;
     });
@@ -73,89 +76,28 @@ class SignAppState extends State<SignApp> {
         builder: (BuildContext context) => FullScreenImage(
               pngBytes: pngBytes,
             )));
-//    showImage(context);
   }
-
-  Future<Null> showImage(BuildContext context) async {
-    var pngBytes = await image.toByteData(format: ui.ImageByteFormat.png);
-//    if(!(await checkPermission())) await requestPermission();
-//    // Use plugin [path_provider] to export image to storage
-//    Directory directory = await getExternalStorageDirectory();
-//    String path = directory.path;
-//    print(path);
-//    await Directory('$path/$directoryName').create(recursive: true);
-//    File('$path/$directoryName/${formattedDate()}.png')
-//        .writeAsBytesSync(pngBytes.buffer.asInt8List());
-//    return showDialog<Null>(
-//        context: context,
-//        builder: (BuildContext context) {
-//          return AlertDialog(
-//            title: Text(
-//              'Please check your device\'s Signature folder',
-//              style: TextStyle(
-//                  fontFamily: 'Roboto',
-//                  fontWeight: FontWeight.w300,
-//                  color: Theme.of(context).primaryColor,
-//                  letterSpacing: 1.1
-//              ),
-//            ),
-//            content: Image.memory(Uint8List.view(pngBytes.buffer)),
-//          );
-//        }
-//    );
-  }
-
-  String formattedDate() {
-    DateTime dateTime = DateTime.now();
-    String dateTimeString = 'Signature_' +
-        dateTime.year.toString() +
-        dateTime.month.toString() +
-        dateTime.day.toString() +
-        dateTime.hour.toString() +
-        ':' +
-        dateTime.minute.toString() +
-        ':' +
-        dateTime.second.toString() +
-        ':' +
-        dateTime.millisecond.toString() +
-        ':' +
-        dateTime.microsecond.toString();
-    return dateTimeString;
-  }
-
-//  requestPermission() async {
-//    bool result = await SimplePermissions.requestPermission(_permission);
-//    return result;
-//  }
-//
-//  checkPermission() async {
-//    bool result = await SimplePermissions.checkPermission(_permission);
-//    return result;
-//  }
-//
-//  getPermissionStatus() async {
-//    final result = await SimplePermissions.getPermissionStatus(_permission);
-//    print("permission status is " + result.toString());
-//  }
-
 }
 
-class Signature extends StatefulWidget {
-  Signature({Key key}) : super(key: key);
+class CustomDrawView extends StatefulWidget {
+  CustomDrawView({Key key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-    return SignatureState();
+    return CustomDrawViewState();
   }
 }
 
-class SignatureState extends State<Signature> {
-  // [SignatureState] responsible for receives drag/touch events by draw/user
+class CustomDrawViewState extends State<CustomDrawView> {
+  // [CustomDrawViewState] responsible for receives drag/touch events by draw/user
   // @_points stores the path drawn which is passed to
-  // [SignaturePainter]#contructor to draw canvas
+  // [CustomDrawViewPainter]#contructor to draw canvas
   List<Offset> _points = <Offset>[];
   ui.Image image;
   bool isImageloaded = false;
+  var paintController;
+  Color activeColor = Color(0xFFFF0000);
+  double strokeHeight = 5.0;
 
   void initState() {
     super.initState();
@@ -165,6 +107,14 @@ class SignatureState extends State<Signature> {
   Future<Null> init() async {
     final ByteData data = await rootBundle.load('images/dummy.jpg');
     image = await loadImage(new Uint8List.view(data.buffer));
+    _initiatePaintController();
+  }
+
+  void _initiatePaintController() {
+    paintController = Paint()
+      ..color = activeColor
+      ..strokeCap = StrokeCap.square
+      ..strokeWidth = strokeHeight;
   }
 
   Future<ui.Image> loadImage(List<int> img) async {
@@ -182,11 +132,12 @@ class SignatureState extends State<Signature> {
     // [CustomPainter] has its own @canvas to pass our
     // [ui.PictureRecorder] object must be passed to [Canvas]#contructor
     // to capture the Image. This way we can pass @recorder to [Canvas]#contructor
-    // using @painter[SignaturePainter] we can call [SignaturePainter]#paint
+    // using @painter[CustomDrawViewPainter] we can call [CustomDrawViewPainter]#paint
     // with the our newly created @canvas
     ui.PictureRecorder recorder = ui.PictureRecorder();
     Canvas canvas = Canvas(recorder);
-    SignaturePainter painter = SignaturePainter(points: _points, image: image);
+    CustomDrawViewPainter painter = CustomDrawViewPainter(
+        points: _points, image: image, paintController: paintController);
     var size = context.size;
     painter.paint(canvas, size);
     return recorder
@@ -214,7 +165,10 @@ class SignatureState extends State<Signature> {
           },
           child: isImageloaded
               ? CustomPaint(
-                  painter: SignaturePainter(points: _points, image: image),
+                  painter: CustomDrawViewPainter(
+                      points: _points,
+                      image: image,
+                      paintController: paintController),
                   size: Size.infinite,
                 )
               : new Center(child: new Text('loading')),
@@ -231,25 +185,64 @@ class SignatureState extends State<Signature> {
       _points.clear();
     });
   }
+
+  void openColorPicker() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          titlePadding: const EdgeInsets.all(0.0),
+          contentPadding: const EdgeInsets.all(0.0),
+          content: SingleChildScrollView(
+//            child: ColorPicker(
+//              pickerColor: activeColor,
+//              onColorChanged: (Color chosenColor) {
+//                setState(() {
+//                  activeColor = chosenColor;
+//                });
+//              },
+//              colorPickerWidth: 300.0,
+//              pickerAreaHeightPercent: 0.7,
+//              enableAlpha: true,
+//              displayThumbColor: true,
+//              showLabel: true,
+//              paletteType: PaletteType.hsv,
+//              pickerAreaBorderRadius: const BorderRadius.only(
+//                topLeft: const Radius.circular(2.0),
+//                topRight: const Radius.circular(2.0),
+//              ),
+//            ),
+            child: ColorPicker(
+              pickerColor: activeColor,
+              onColorChanged: (Color chosenColor) {
+                setState(() {
+                  activeColor = chosenColor;
+                });
+                _initiatePaintController();
+              },
+              showLabel: true,
+              pickerAreaHeightPercent: 0.9,
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
 
-class SignaturePainter extends CustomPainter {
-  // [SignaturePainter] receives points through constructor
+class CustomDrawViewPainter extends CustomPainter {
+  // [CustomDrawViewPainter] receives points through constructor
   // @points holds the drawn path in the form (x,y) offset;
   // This class responsible for drawing only
   // It won't receive any drag/touch events by draw/user.
   List<Offset> points = <Offset>[];
   ui.Image image;
+  var paintController;
 
-  SignaturePainter({this.points, this.image});
+  CustomDrawViewPainter({this.points, this.image, this.paintController});
 
   @override
   void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.blue
-      ..strokeCap = StrokeCap.square
-      ..strokeWidth = 5.0;
-
     final outputRect =
         Rect.fromPoints(ui.Offset.zero, ui.Offset(size.width, size.height));
     final Size imageSize =
@@ -260,17 +253,17 @@ class SignaturePainter extends CustomPainter {
         Alignment.center.inscribe(sizes.source, Offset.zero & imageSize);
     final Rect outputSubrect =
         Alignment.center.inscribe(sizes.destination, outputRect);
-    canvas.drawImageRect(image, inputSubrect, outputSubrect, paint);
+    canvas.drawImageRect(image, inputSubrect, outputSubrect, paintController);
 
     for (int i = 0; i < points.length - 1; i++) {
       if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i], points[i + 1], paint);
+        canvas.drawLine(points[i], points[i + 1], paintController);
       }
     }
   }
 
   @override
-  bool shouldRepaint(SignaturePainter oldDelegate) {
+  bool shouldRepaint(CustomDrawViewPainter oldDelegate) {
     return oldDelegate.points != points;
   }
 }
@@ -288,5 +281,3 @@ class FullScreenImage extends StatelessWidget {
     ));
   }
 }
-
-
